ec06837ed2a75365f0682abd15930b45d9c74117
yunis91 <yunis.mamedov@orange-yc.de>
2026-02-12 08:06:33 +0100

    add ui to AuthForm

--- front-end/src/features/auth/ui/AuthForm.tsx
+++ front-end/src/features/auth/ui/AuthForm.tsx
@@ -1,28 +1,57 @@
 'use client'
 
+import { Button } from '@/shared/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/shared/components/ui/card'
+import { Input } from '@/shared/components/ui/input'
+import { Label } from '@/shared/components/ui/label'
+
 interface Props {
   type: 'login' | 'register'
 }
 
 export function AuthForm({ type }: Props) {
   return (
-    <div>
-      <h1>{type === 'register' ? 'Register' : 'Login'}</h1>
-      <form>
-        <input
-          type="text"
-          name="email"
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          name="passrowd"
-          placeholder="Password"
-        />
-        <button type="submit">
+    <Card className="mx-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle>{type === 'register' ? 'Register' : 'Login'}</CardTitle>
+      </CardHeader>
+      <CardContent>
+        <form>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                id="password"
+                type="password"
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter>
+        <Button
+          type="submit"
+          className="w-full"
+        >
           {type === 'register' ? 'Register' : 'Login'}
-        </button>
-      </form>
-    </div>
+        </Button>
+      </CardFooter>
+    </Card>
   )
 }
