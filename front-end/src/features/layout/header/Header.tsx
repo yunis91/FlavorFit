f09394d89b94a738f813d85b20d2d742c53a2ac4
yunis91 <yunis.mamedov@orange-yc.de>
2026-02-18 15:46:11 +0100

    changes day 22

--- front-end/src/features/layout/header/Header.tsx
+++ front-end/src/features/layout/header/Header.tsx
@@ -3,10 +3,14 @@
 import { NavMenu } from '@/shared/components/custom-ui/nav-menu/NavMenu'
 
 import { navMenuItems } from './nav.data'
+import { IconLogo } from '@/shared/ui/icons/IconLogo'
 
 export function Header() {
   return (
-    <header className="p-5">
+    <header className="flex items-center gap-3.5 p-5">
+      <div className="flex size-9 cursor-pointer items-center justify-center rounded-full bg-linear-to-t from-[#7659eb] to-[#977eff] transition-transform duration-150 ease-in-out hover:scale-105">
+        <IconLogo className="size-4 text-white" />
+      </div>
       <NavMenu menu={navMenuItems} />
     </header>
   )
